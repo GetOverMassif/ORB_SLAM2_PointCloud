cee3b466a000a435f6a88ff544e3e45cb55fa7af
GetOverMassif <164567487@qq.com>
2022-09-21 00:09:27 +0800

    orb with pointcloud

---
+++ script/rgbd_tum1.bash
@@ -0,0 +1,9 @@
+###
+ # @Author: GetOverMassif 164567487@qq.com
+ # @Date: 2022-09-20 20:07:44
+ # @LastEditors: GetOverMassif 164567487@qq.com
+ # @LastEditTime: 2022-09-20 20:07:47
+ # @FilePath: /ORB_SLAM2_modified/script/rgbd_tum1.bash
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+./bin/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml /media/lj/TOSHIBA/dataset/TUM/2-Handheld_SLAM/rgbd_dataset_freiburg1_desk /media/lj/TOSHIBA/dataset/TUM/2-Handheld_SLAM/rgbd_dataset_freiburg1_desk/associations.txt
